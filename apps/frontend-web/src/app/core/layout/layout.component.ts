f5ec28823ce2260956236440358577a650266081
shalom <shalomlee6@gmail.com>
2026-02-06 19:21:33 +0200

    dashboard and login screens

--- apps/frontend-web/src/app/core/layout/layout.component.ts
+++ apps/frontend-web/src/app/core/layout/layout.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
